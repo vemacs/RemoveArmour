ab41a4842cd13a2b30424fb648941c1e54d135e1
Wundark <weavp001@gmail.com>
2013-04-09 23:34:19 +0100

    Thanks to evilmidget38 for fixes. Added permission node for player usage.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 A simple plugin that adds functionality for Command Blocks to remove armour of a player.
 
 Syntax is /removearmour [Player] (The player can have a part spelt name, for example if my username was Wundark I could have the command as /removearmour Wun)
+
+Permission node:
+- removearmour.use ~ Allows for the usage of the command.
--- eu/wServers/RemoveArmour/RemoveArmour.java
+++ eu/wServers/RemoveArmour/RemoveArmour.java
@@ -1,7 +1,6 @@
 package eu.wServers.RemoveArmour;
 
 import org.bukkit.ChatColor;
-import org.bukkit.command.BlockCommandSender;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -9,29 +8,30 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class RemoveArmour extends JavaPlugin
 {
-	String bad = ChatColor.GOLD + "[RemourArmour] " + ChatColor.RED;
-	
-	public void onEnable()
-	{
-		getCommand("removearmour").setExecutor(this);
-	}
+	String bad = ChatColor.GOLD + "[RemoveArmour] " + ChatColor.RED;
 	
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) 
 	{
-		if(command.getName().equalsIgnoreCase("removearmour"))
+		if(sender instanceof Player)
 		{
-			if(!(sender instanceof BlockCommandSender))
+			if(sender.hasPermission("removearmour.use"))
 			{
-				sender.sendMessage(bad + "Command blocks only.");
+				sender.sendMessage(bad + "You do not have permissions to use this command.");
 				return true;
 			}
-			
-			Player player = getServer().getPlayer(args[0]);
-			
-			if(player != null)
-			{
-				player.getInventory().setArmorContents(null);
-			}
+		}
+		
+		if(args.length != 1)
+		{
+			sender.sendMessage(bad + "Syntax: /removearmour [Player]");
+			return true;
+		}
+		
+		Player player = getServer().getPlayer(args[0]);
+		
+		if(player != null)
+		{
+			player.getInventory().setArmorContents(null);
 		}
 		
 		return true;
